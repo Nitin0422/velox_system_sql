a38ed75abd7950d457f8943edb880cfe19040131
Nitin Tandukar <nitintandukar@Nitins-MacBook-Air.local>
2023-10-14 13:04:10 +0545

    Redefined schema to include user and employes in one table

--- my_app/models.py
+++ my_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import os
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Department(models.Model):
@@ -17,17 +18,24 @@ class DepartmentGroup(models.Model):
     def __str__(self):
         return self.group_name
 
-class Employee(models.Model):
-    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
-    group = models.ForeignKey(DepartmentGroup, on_delete=models.DO_NOTHING)
-    employee_full_name = models.CharField(max_length=200)
-    employee_contact_number = models.CharField(max_length=200)
-    employee_address = models.CharField(max_length=200)
-    employee_PAN = models.CharField(max_length=200)
-    email = models.EmailField()
+class Staff(AbstractUser): #inherits all the fields present in the default user. 
+    address = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    PAN = models.CharField(max_length=200)
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
 
     def __str__(self):
-        return self.employee_full_name
+        return self.first_name + " " + self.first_name
+
+class StaffAssociation(models.Model):
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
+    group = models.ForeignKey(DepartmentGroup, on_delete=models.DO_NOTHING)
+    
+    def __str__(self):
+        return self.staff.first_name + "-" + self.department.department_name + "-" + self.group.group_name
+
 
 class Customer(models.Model):
     customer_full_name = models.CharField(max_length=200)
@@ -58,7 +66,7 @@ def validate_file_extension(value):
 
 class Invoice(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.DO_NOTHING)
-    employee = models.ForeignKey(Employee, on_delete=models.DO_NOTHING)
+    employee = models.ForeignKey(Staff, on_delete=models.DO_NOTHING)
     task_code = models.ForeignKey(TaskCode, on_delete=models.DO_NOTHING)
     deal_amount = models.FloatField()
     paid_amount = models.FloatField()
