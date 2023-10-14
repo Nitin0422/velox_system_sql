f122ae0eae434cb999ea20cb920572101ddb1df5
Nitin Tandukar <nitintandukar@Nitins-MacBook-Air.local>
2023-10-14 16:55:14 +0545

    Managed user access control in sidenav

--- my_app/context_processors.py
+++ my_app/context_processors.py
@@ -1,10 +1,16 @@
 def site_settings(request):
     is_admin = False
+    is_department_admin =False
+    is_staff = False
     
     
     if request.user.is_authenticated:
         is_admin = request.user.groups.filter(name='Admin').exists()
+        is_department_admin = request.user.groups.filter(name="DepartmentAdmin").exists()
+        is_staff = request.user.groups.filter(name = "Staff").exists()
 
     return {
         "is_admin": is_admin,
+        "is_department_admin": is_department_admin,
+        "is_staff": is_staff
     }
