25293dadce1edae1e42cb9eb92f6c25260692ce3
Nitin Tandukar <nitintandukar@Nitins-MacBook-Air.local>
2023-10-14 13:05:08 +0545

    Configured user creation form

--- my_app/forms.py
+++ my_app/forms.py
@@ -1,27 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Invoice
+from .models import Invoice, Staff
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField()
-    
     class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-    def save(self, commit = True):
-        user = super(RegistrationForm, self).save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit: 
-            user.save()
-        return user
-    
+        model = Staff
+        fields = ('first_name', 'last_name', 'phone_number', 'address', 'PAN', 'email', 'username', 'password1', 'password2')
+  
 class InvoiceForm(forms.ModelForm):
     class Meta:
         model = Invoice
         fields = ['task_code', 'deal_amount', 'paid_amount', 'due_amount', 'bill_file', 'invoice_date' ]
-        
+
     def __init__(self, *args, **kwargs):
         super(InvoiceForm, self).__init__(*args, **kwargs)
         self.fields['task_code'].widget.attrs.update({'class': 'form-select'})
